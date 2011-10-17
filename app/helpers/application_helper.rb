e5c858b0115cc5d288580c7357ceb86803601735
Steve Roettger <steve@steveroettger.com>
2011-10-17 17:32:41 -0400

    Added layouts, controllers, links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+	# Return a title on a per-page basis.
+	def title
+		base_title = "Kurbi"
+		if @title.nil?
+		base_title
+		else
+		 "#{base_title} | #{@title}"
+		end
+	end	 
+	
+	def logo
+		image_tag("logo.png", :alt => "Kurbi", :class => "round")
+	end
+
 end
