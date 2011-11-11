d4c00f003ffcf1b107e1e6beeccb7659441ea328
Steve Roettger <steve@steveroettger.com>
2011-11-11 16:09:40 -0500

    Modified user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#  admin              :boolean         default(FALSE)
+#
+
 class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
@@ -19,6 +33,7 @@ before_save :encrypt_password
 			encrypted_password == encrypt(submitted_password)
 		end
 	
+	
 		def self.authenticate(email, submitted_password)
 		    user = find_by_email(email)
 		    return nil  if user.nil?
@@ -29,7 +44,7 @@ before_save :encrypt_password
 		    user = find_by_id(id)
 		    (user && user.salt == cookie_salt) ? user : nil
 		end
-				
+			
 	private
 		def encrypt_password
 			self.salt = make_salt if new_record?
@@ -50,17 +65,4 @@ before_save :encrypt_password
 end
 
 
-# == Schema Information
-#
-# Table name: users
-#
-#  id                 :integer         not null, primary key
-#  name               :string(255)
-#  email              :string(255)
-#  created_at         :datetime
-#  updated_at         :datetime
-#  encrypted_password :string(255)
-#  salt               :string(255)
-#  admin              :boolean         default(FALSE)
-#
 
