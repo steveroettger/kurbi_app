e15037275b61da12f147486c69a7366e1d846b14
Steve Roettger <steve@steveroettger.com>
2012-01-28 14:57:30 -0500

    Modified the spec folder

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -20,36 +20,4 @@ require 'spec_helper'
 
 describe Profile do
   
-  before(:each) do
-  	@user = Factory(:user)
-  	@attr = {
-  		:location     =>     "example location", 
-  		:bio          =>     "bio of the user", 
-  		:birthday     =>     "00/00/0000", 
-  		:diagnosed    =>     "0000", 
-  		:medications  =>     "medications user is taking", 
-  		:doctor       =>     "doctor",
-  		:sex          =>     "male",
-  		:race         =>     "caucasian"
-   		}
-  end
-  
-  it "should create a new instance given valid attributes" do
-  	@user.create_profile
-  end
-  
-  describe "user & profile associations" do
-  	before(:each) do	
-  		@profile = @user.create_profile
-  	end
-  	
-  	it "should have a user attribute (id)" do
-  		@profile.should respond_to(:user)
-  	end
-  	
-  	it "should have the right associated user, to the correct profile" do
-  		@profile.user_id.should == @user.id
-  		@profile.user.should == @user
-  	end
-  end
 end
